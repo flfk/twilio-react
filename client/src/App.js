2734f4879f211ed4c65f7de46c382a00ed34f8f9
flfk <flfk@users.noreply.github.com>
2018-09-27 10:58:30 -0300

    Initial base expressgenerator server with create-react-app set up to return users

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    users: [],
+  }
+
+  componentDidMount() {
+      fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }))
+  }
+
+  render() {
+    const {users} = this.state
+
+    return (
+      <div className="App">
+      <h1>Users</h1>
+      {users.map(user => <div>{user.user}</div>)}
+      </div>
+    );
+  }
+}
+
+export default App;
